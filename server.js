16ecc0340a343d9d15387604ce7ea5fc33961ec8
vrrdnt <will.arctic@gmail.com>
2024-01-27 15:26:01 -0600

    ios

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sharp = require('sharp');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use('/downloads', express.static(path.join(__dirname, 'downloads')));
